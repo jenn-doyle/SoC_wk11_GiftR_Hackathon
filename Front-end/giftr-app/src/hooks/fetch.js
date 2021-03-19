dcc876f9a4f676c4ee7aee1c74562a1e51056833
Jenn <jenndoyle@hotmail.co.uk>
2021-03-19 15:33:12 +0000

    fetch

--- Front-end/giftr-app/src/hooks/fetch.js
+++ Front-end/giftr-app/src/hooks/fetch.js
@@ -1,10 +1,10 @@
-import React, { useReducer, useEffect } from "react";
-import "./index.css";
+import React, { useReducer, useEffect } from 'react';
+import './index.css';
 
 //reducer only accepts action of type Gift, with a payload of the actual gift
 function reducer(state, action) {
   switch (action.type) {
-    case "GIFT":
+    case 'GIFT':
       return action.payload;
     default:
       return state;
@@ -21,22 +21,40 @@ function GiftViewer({ id }) {
     async function getGifts() {
       let res = await fetch(`${BACKEND_URL}/${id}`); //id might be wrong
       let data = await res.json();
-      dispatch({ type: "GIFT", payload: data });
+      dispatch({ type: 'GIFT', payload: data });
       console.log(gift);
     }
     getGifts();
   }, [id]);
 
   return (
-    <div  className="gift-viewer">
-      <ul>
-        <li> {gift.person} </li>
-        <li> {gift.giftname} </li>
-        <li> {gift.giftlink} </li>
-        <li> {gift.imagelink} </li>
-      </ul>
+    <div className="gift-viewer">
+      <p>{gift}</p>
     </div>
   );
 }
 
 export default GiftViewer;
+
+
+// import { useEffect, useState } from "react";
+
+// function useFetch() {
+//   const [dataState, setDataState] = useState(1);
+//  
+//   const BACKEND_URL = "https:localhost:5001/gifts";
+
+//   async function fetchData() {
+//     let result = await fetch(`${BACKEND_URL}`);
+//     let data = await result.json();
+//     setDataState(data);
+//   }
+
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   return dataState;
+// }
+
+// export default useFetch;
