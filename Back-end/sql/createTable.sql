ee4019e21995219b676e47290684f274dee5a78d
Roxana Tot <roxana.tot@gmail.com>
2021-03-19 11:11:50 +0000

    User secrets initiated

--- Back-end/sql/createTable.sql
+++ Back-end/sql/createTable.sql
@@ -1,18 +1,16 @@
-CREATE TABLE Songs (
+CREATE TABLE Gifts (
     Id SERIAL PRIMARY KEY,
-    Title TEXT,
-    Artist TEXT,
-    SongLengthCode INT,
-    Link VARCHAR,
-    SuggestedBy TEXT
+    Person TEXT,
+    GiftName TEXT,
+    ImageLink TEXT,
+    GiftLink TEXT
 );
 
 INSERT INTO
-    Songs (Title, Artist, SongLengthCode, Link, SuggestedBy)
+    Gifts (Person, GiftName, ImageLink, GiftLink)
 VALUES
---  //  Create code for time buckets (1 = 0-60 secs)(2 =61-90 )(3 =91-120 )(4 =121-150 )(5=151-180)(6=181-210)(7=211-240)(8=241-270) - must be converted on Input Componenets - front end 
-    ('Save Your Tears', 'The Weekend', 8, 'https://www.youtube.com/watch?v=XXYlFuWEuKI&list=RDCLAK5uy_mkLtojKLOUUGwnu3ZnN5AaODijlieB-aQ&index=5', 'Craig Farrell'),
-    ('We`re Good', 'Dua Lipa', 6, 'https://www.youtube.com/watch?v=jr47YisIsz8&list=RDCLAK5uy_mkLtojKLOUUGwnu3ZnN5AaODijlieB-aQ&index=11', 'Craig Farrell'),
-    ('Red Alert', 'Basement Jaxx', 7, 'https://www.youtube.com/watch?v=SJyhZ-3Z8A8', 'Jenn Doyle'),
-    ('Go Your Own Way', 'Fleetwood Mac', 7, 'https://www.youtube.com/watch?v=6ul-cZyuYq4', 'Jenn Doyle'),
-    ('Let`s Dance', 'David Bowie', 8, 'https://www.youtube.com/watch?v=VbD_kBJc_gI', 'Bob Bobson III');
+    ('Mom', 'Book', 'https://images.unsplash.com/photo-1519682337058-a94d519337bc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80', 'https://www.waterstones.com/blog/the-best-books-for-new-mums'),
+    ('Dad', 'Book', 'https://images.unsplash.com/photo-1519682337058-a94d519337bc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80', 'https://www.waterstones.com/blog/the-best-books-for-new-mums'),
+    ('Sister', 'Book', 'https://images.unsplash.com/photo-1519682337058-a94d519337bc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80', 'https://www.waterstones.com/blog/the-best-books-for-new-mums'),
+    ('Friend', 'Book', 'https://images.unsplash.com/photo-1519682337058-a94d519337bc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80', 'https://www.waterstones.com/blog/the-best-books-for-new-mums'),
+    ('Acquaintance', 'Book', 'https://images.unsplash.com/photo-1519682337058-a94d519337bc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80', 'https://www.waterstones.com/blog/the-best-books-for-new-mums');
