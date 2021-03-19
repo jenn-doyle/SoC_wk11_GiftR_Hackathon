0adf9b3ff54b3d864187485568e3ebc3a2dfaeac
Stella Taylor <stellataylor10@gmail.com>
2021-03-19 11:10:10 +0000

    router done

---
+++ Front-end/giftr-app/src/About/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+    return (
+        <div>
+        <h1>ABOUT US</h1>
+        <p>GIFTR is a leading search engine of gifts for you and your family family. We are dedicated to ensuring that your family have the latest and most innovative personal gifts.
+Our extensive range of products spans luxury gift sets to inexpensive items. They are suitable for all requirements from birthdays, festivities or anniversaries. 
+GIFTR is committed to our customers satisfaction, and we want to ensure that your family have the perfect gift.
+</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ Front-end/giftr-app/src/Main/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+    return (
+        <div>
+        <h1>GIFTR suggestions</h1>
+       {/* import Buttons and List Components (which render on button click) */}
+        </div>
+    )
+}
+
+export default Main;
